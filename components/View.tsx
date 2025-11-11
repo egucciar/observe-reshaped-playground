41cc357e61396893b8e01dcf802582665c03a496
Erica Gucciardo <erica.gucciardo@observeinc.com>
2025-11-11 18:42:58 -0500

    extending view types breaks as casting

--- components/View.tsx
+++ components/View.tsx
@@ -1,2 +1,18 @@
-export { View } from 'reshaped'
-export type { ViewProps } from 'reshaped'
+import { View as ReshapedView, type ViewProps as ReshapedViewProps } from 'reshaped'
+
+export interface ViewProps extends ReshapedViewProps {}
+
+export const View = ({ ...props }: ViewProps) => {
+  return <ReshapedView {...props} />
+}
+
+const Example = () => {
+  return (
+    <View as="span">
+      This doesn't pass type checks , so it feels like as="span" doesn't work!
+    </View>
+  )
+}
+
+View.displayName = 'View'
+View.Item = ReshapedView.Item
