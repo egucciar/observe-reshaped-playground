ba2a2b220f996960a6a4518a1f0289d045c0bbee
Erica Gucciardo <erica.gucciardo@observeinc.com>
2025-11-26 16:15:07 -0500

    minor update to playground page

--- views/PlaygroundPage.tsx
+++ views/PlaygroundPage.tsx
@@ -1,15 +1,17 @@
-import { View } from '../components/View'
-import { Text } from '../components/Text'
+'use client';
+
+import { Text } from '../components/Text';
+import { View } from '../components/View';
 
 export function PlaygroundPage() {
   return (
     <View padding={6} gap={4} maxWidth="1200px">
       <View gap={2}>
-        <Text variant="title-1">Hello World!</Text>
+        <Text variant="title-1">Playground</Text>
         <Text variant="body-1" color="neutral-faded">
           This is your playground - use this space to experiment and try things out!
         </Text>
       </View>
     </View>
-  )
+  );
 }
